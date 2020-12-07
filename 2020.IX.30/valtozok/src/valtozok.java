9460f2098f15e94937d9e5d1304f260dfdc9482b
18aKissD <75607714+18aKissD@users.noreply.github.com>
2020-12-07 10:59:17 +0100

    codes

---
+++ 2020.IX.30/valtozok/src/valtozok.java
@@ -0,0 +1,18 @@
+package valtozok   
+   
+public class Valtozok
+
+        public static void main(String [] args) {
+            
+            //Változók deklarálása//
+            
+            int sebessegFokozat;
+            double sebesseg;
+            char szin;
+            boolean metal
+
+
+            //Programcím kiiratása
+
+            System.out.println("Ez egy virtuális autó")
+}
